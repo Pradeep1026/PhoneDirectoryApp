6f6da5235de57f608493b96bbac0f212fa516a9f
Pradeep <guptapradeep1026@gmail.com>
2019-03-14 17:34:30 +0530

    Updates

---
+++ precache-manifest.5bffb761bd8585791772a0d5374cfab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d60cd1de700b71c550a",
+    "url": "/PhoneDirectoryApp/static/js/runtime~main.b85a2de9.js"
+  },
+  {
+    "revision": "a8a65d0557a34dc16a3c",
+    "url": "/PhoneDirectoryApp/static/js/main.78986251.chunk.js"
+  },
+  {
+    "revision": "63579d506474af524905",
+    "url": "/PhoneDirectoryApp/static/js/2.5f8e58a3.chunk.js"
+  },
+  {
+    "revision": "a8a65d0557a34dc16a3c",
+    "url": "/PhoneDirectoryApp/static/css/main.ed5f6f1f.chunk.css"
+  },
+  {
+    "revision": "ef1aab0cc6215437cc5eb5e11c960be0",
+    "url": "/PhoneDirectoryApp/index.html"
+  }
+];
